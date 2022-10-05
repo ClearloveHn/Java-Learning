a13b09def8397c3f8ffc55819e56686acf032b3a
lhn19 <28033640@qq.com>
2022-10-05 14:55:24 +0800

    面向对象

---
+++ objectProj/animal/Cat.java
@@ -0,0 +1,23 @@
+package objectProj.animal;
+
+/**
+ * 宠物猫类(Class)
+ */
+public class Cat {
+    //属性
+    String name;
+    int month;
+    double weight;
+    String species;
+
+    //行为: run && eat
+    public void run() {
+        System.out.println("小猫会跑");
+    }
+
+    public void eat() {
+        System.out.println("小猫吃鱼");
+    }
+
+
+}
---
+++ objectProj/animal/CatTest.java
@@ -0,0 +1,11 @@
+package objectProj.animal;
+
+public class CatTest {
+    public static void main(String[] args) {
+        //对象实例化操作
+        Cat one = new Cat();
+        //调用对象的方法
+        one.eat();
+        one.run();
+    }
+}
