8845e9b92796418dac707a93d5ea41b4b12080ef
lhn19 <28033640@qq.com>
2022-10-18 22:06:37 +0800

    for增强型循环和forEach

--- Collection/LinkedListSample.java
+++ Collection/LinkedListSample.java
@@ -1,6 +1,9 @@
 package Collection;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LinkedListSample {
     public static void main(String[] args) {
@@ -9,5 +12,18 @@ public class LinkedListSample {
         bookList.addFirst("江南");
         bookList.addLast("龙族2");
         System.out.println(bookList);
+
+        List<String> countryList = new ArrayList<>();
+        countryList.add("CH");
+        countryList.add("MY");
+        countryList.add("SG");
+        countryList.add("AU");
+        for (String country:countryList) {
+            System.out.println(country);
+        }
+        System.out.println("===================");
+        countryList.forEach(country->{
+            System.out.println(country);
+        });
     }
 }
