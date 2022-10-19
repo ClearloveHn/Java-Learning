b278702980ca32d2ce2a2e5b8a504375117b2f32
lhn19 <28033640@qq.com>
2022-10-19 10:35:14 +0800

    优化

--- Collection/TreeSetSample.java
+++ Collection/TreeSetSample.java
@@ -5,29 +5,27 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class TreeSetSample {
-        //自定义Comparator
-        class IntegerCompare implements Comparator<Integer> {
+    //自定义Comparator
+    class IntegerCompare implements Comparator<Integer> {
 
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1-o2;
-            }
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o1 - o2;
         }
-
-        public void sort() {
-            Set<Integer> set = new TreeSet<>(new IntegerCompare());
-            set.add(1);
-            set.add(2);
-            set.add(5);
-            set.add(4);
-            System.out.println(set);
-        }
-    public static void main(String[] args) {
-       TreeSetSample treeSetSample = new TreeSetSample();
-       treeSetSample.sort();
-
     }
 
+    public void sort() {
+        Set<Integer> set = new TreeSet<>(new IntegerCompare());
+        set.add(1);
+        set.add(2);
+        set.add(5);
+        set.add(4);
+        System.out.println(set);
+    }
 
+    public static void main(String[] args) {
+        TreeSetSample treeSetSample = new TreeSetSample();
+        treeSetSample.sort();
 
+    }
 }
