17bc0df666c495cac13b7b1ab14ff33c2150cbb7
lhn19 <28033640@qq.com>
2022-10-19 09:43:45 +0800

    优化

--- Collection/ArrayListSample.java
+++ Collection/ArrayListSample.java
@@ -2,15 +2,17 @@ package Collection;
 
 import java.util.ArrayList;
 
+
 public class ArrayListSample {
     public static void main(String[] args) {
         ArrayList<String> bookList = new ArrayList<>();
         bookList.add("三国演义");
         bookList.add("水浒传");
+
         System.out.println(bookList.get(0));
         String bookName = bookList.get(1);
         System.out.println(bookName);
-        bookList.set(1,"西游记");
+        bookList.set(1, "西游记");
         System.out.println(bookList.get(1));
         bookList.remove(0);
         System.out.println(bookList);
@@ -18,5 +20,6 @@ public class ArrayListSample {
         String item = bookList.remove(0);
         System.out.println(item);
         System.out.println(bookList);
+
     }
 }
--- Collection/LinkedListSample.java
+++ Collection/LinkedListSample.java
@@ -1,6 +1,5 @@
 package Collection;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
